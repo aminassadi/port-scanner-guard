67e61e9a188159ab64e65e4b97b60c4fc67cdf07
Aminassadi <aminassadi.og@gmail.com>
2024-08-28 11:16:25 -0400

    [UPD] gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bpf/libbpf.h>
+#include "pscannerguard.skel.h"
 
 static void handle_terminate_signal(int sig)
 {
@@ -7,5 +8,26 @@ static void handle_terminate_signal(int sig)
 
 int main(int argc, char **argv)
 {
+    auto skel = pscannerguard_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+	}
+
+
+	auto err = pscannerguard_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		return 0;
+	}
+
+	/* Attach tracepoints */
+	err = pscannerguard_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		return 0;
+	}
+    std::cin.get();
+    pscannerguard_bpf__destroy(skel);
     return 0;
 }
