44f28e1115d1e2908082c7907c7fbb6485230527
Aminassadi <aminassadi.og@gmail.com>
2024-09-12 04:18:11 -0400

    [WIP] handle half open sock

--- src/kern/bpf.c
+++ src/kern/bpf.c
@@ -1,17 +1,197 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
 
-/// @ifindex 1
-/// @flags 0
-/// @xdpopts {"old_prog_fd":0}
+#ifndef ETH_P_IP
+#define ETH_P_IP 0x0800
+#endif
+#ifndef ETH_P_IPV6
+#define ETH_P_IPV6 0x86DD
+#endif
+#ifndef ETH_P_ARP
+#define ETH_P_ARP 0x0806
+#endif
+#ifndef ETH_P_PPP_MP
+#define ETH_P_PPP_MP 0x0008
+#endif /* Dummy type for PPP MP frames */
+
+#define MAX_HALF_OPEN_SOCKET 1024 * 1024
+#define HALF_OPEN_SOCKET_LIMIT 10
+
+struct tracepoint__sock__inet_sock_set_state
+{
+	uint64_t pad;
+	const void *skaddr; // kernel struct sock *
+	int oldstate;
+	int newstate;
+	__u16 sport;
+	__u16 dport;
+	__u16 family;
+	__u16 protocol;
+	__u8 saddr[4];
+	__u8 daddr[4];
+	__u8 saddr_v6[16];
+	__u8 daddr_v6[16];
+};
+
+struct
+{
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_HALF_OPEN_SOCKET);
+	__type(key, __be32);
+	__type(value, int);
+} half_open_socket_map SEC(".maps");
+
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(key_size, sizeof(int));
+    __uint(value_size, sizeof(uint32_t));
+    __uint(max_entries, 100 * 1024);
+} perf_buff SEC(".maps");
+
+typedef enum
+{
+	FAMILY_AF_INET = 2,
+	FAMILY_AF_INET6 = 10
+} family;
+
 SEC("xdp")
-int xdp_pass(struct xdp_md* ctx) {
-    void* data = (void*)(long)ctx->data;
-    void* data_end = (void*)(long)ctx->data_end;
-    int pkt_sz = data_end - data;
+int xdp_pass(struct xdp_md *ctx)
+{
+	//return XDP_PASS;
+	int pkt_len = 0, map_index;
+	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	uint32_t size = data_end - data;
+	if (size != 60)
+	 	return XDP_PASS;
+	//bpf_printk("size = %d\n", size);
+	struct ethhdr *eth = data;
+	//
+	// data plane 
+	//
 
-    bpf_printk("packet size is %d", pkt_sz);
-    return XDP_PASS;
+
+	struct iphdr *ip;
+	long *value;
+	struct ipv6hdr h;
+	pkt_len = sizeof(*eth);
+	ip = data + pkt_len;
+	pkt_len += sizeof(struct iphdr);
+	if (data + pkt_len > data_end)
+		return XDP_DROP;
+	// map_index = ip->protocol;
+	// if (bpf_ntohs(eth->h_proto) == ETH_P_IP)
+	// 	bpf_printk("rcv packet protocol ipv4\n");
+	// else if (bpf_ntohs(eth->h_proto) == ETH_P_IPV6)
+	// 	bpf_printk("rcv packet protocol ipv6\n");
+	// else if (bpf_ntohs(eth->h_proto) == ETH_P_ARP)
+	// {
+	// 	// bpf_printk("rcv packet protocol ARP\n");
+	// 	return XDP_PASS;
+	// }
+	// else if ((bpf_ntohs(eth->h_proto) == ETH_P_PPP_MP))
+	// {
+	// 	bpf_printk("rcv packet protocol MULTI FRAME PROTOCOL\n");
+	// }
+	// else
+	// {
+	// 	bpf_printk("rcv packet protocol id:%d\n", bpf_ntohs(eth->h_proto));
+	// }
+	if(bpf_ntohl(ip->daddr) != 0xc0a8e783)
+		return XDP_PASS;
+	if (ip->protocol == IPPROTO_ICMP)
+	{
+		bpf_printk("rcv packet at queue %u, ip = %x\n", ctx->rx_queue_index, bpf_ntohl(ip->daddr));
+	}
+	else if (ip->protocol == IPPROTO_TCP)
+	{
+		struct tcphdr *tcp = data + pkt_len;
+		pkt_len += sizeof(struct tcphdr);
+		if (data + pkt_len > data_end)
+			return XDP_DROP;
+		bpf_printk("rcv tcp packet at queue %u, ip= %x, dport= %d, sport= %d\n", ctx->rx_queue_index, bpf_ntohl(ip->daddr), bpf_ntohs(tcp->dest), tcp->source);
+	}
+	else if (ip->protocol == IPPROTO_UDP)
+	{
+		struct udphdr *udp = data + pkt_len;
+		pkt_len += sizeof(struct udphdr);
+		if (data + pkt_len > data_end)
+			return XDP_DROP;
+		bpf_printk("rcv packet at queue %u, ip = %x\n", ctx->rx_queue_index, bpf_ntohl(ip->daddr));
+	}
+	return XDP_PASS;
 }
 
+SEC("tracepoint/sock/inet_sock_set_state")
+__attribute__((flatten)) int TCPconnection(struct tracepoint__sock__inet_sock_set_state *args)
+{
+	// if (args->oldstate == TCP_LISTEN && args->newstate == TCP_SYN_RECV)
+	// {
+	// 	if (args->family == FAMILY_AF_INET)
+	// 	{
+	// 		int *nhsock = NULL;
+	// 		nhsock = bpf_map_lookup_elem(&half_open_socket_map, (int *)args->daddr);
+	// 		if (nhsock == NULL)
+	// 		{
+	// 			int num = 1;
+	// 			if (bpf_map_update_elem(&half_open_socket_map, (int *)args->daddr, &num, BPF_ANY) != 0)
+	// 			{
+	// 				bpf_printk("update half socket map failed with error\n");
+	// 			}
+	// 		}
+	// 		else
+	// 		{
+	// 			*nhsock++;
+	// 			if (*nhsock > HALF_OPEN_SOCKET_LIMIT)
+	// 			{
+	// 				// todo: block ip address
+	// 				return 0;
+	// 			}
+	// 			else
+	// 			{
+	// 				if (bpf_map_update_elem(&half_open_socket_map, (int *)args->daddr, nhsock, BPF_ANY) != 0)
+	// 				{
+	// 					bpf_printk("update half socket map failed with error\n");
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		// todo: handle ipv6
+	// 		return 0;
+	// 	}
+	// }
+	// if(args->dport == 8000)
+	// 	return 0;
+	// if (args->newstate == TCP_SYN_SENT || args->oldstate == TCP_SYN_SENT)
+	// 	return 0;
+	bpf_printk("state changed, socket_addr:%ld, dport:%d, old_state:%d, new_state:%d \n", (int64_t)args->skaddr, args->dport, args->oldstate, args->newstate);
+	if ((args->newstate == TCP_SYN_RECV && args->oldstate == TCP_CLOSE))
+	{
+	}
+	else if (args->newstate == TCP_ESTABLISHED)
+	{
+	}
+	else
+	{
+		return 0;
+	}
+	return 0;
+}
+
+SEC("tracepoint/sock/inet_sk_error_report")
+__attribute__((flatten)) int sock_error(void*args)
+{
+	bpf_printk("err\n");
+}
+
+SEC("tracepoint/tcp/tcp_retransmit_synack")
+__attribute__((flatten)) int tcp_synack(void*args)
+{
+	bpf_printk("retransmit synack\n");
+}
 char __license[] SEC("license") = "GPL";
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bpf/libbpf.h>
 #include "pscannerguard.skel.h"
+#include <net/if.h>
+#include "uapi/linux/if_link.h"
 
 static void handle_terminate_signal(int sig)
 {
@@ -8,26 +10,52 @@ static void handle_terminate_signal(int sig)
 
 int main(int argc, char **argv)
 {
-    auto skel = pscannerguard_bpf__open();
-	if (!skel) {
+	auto skel = pscannerguard_bpf__open();
+	if (!skel)
+	{
 		fprintf(stderr, "Failed to open and load BPF skeleton\n");
 		return 1;
 	}
 
-
 	auto err = pscannerguard_bpf__load(skel);
-	if (err) {
+	if (err)
+	{
 		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
 		return 0;
 	}
 
+	auto ifindex_list = if_nametoindex("ens33");
+	auto xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_SKB_MODE;
+	// if (strncmp(argv[2], "--skb-mode", strlen(argv[2])) == 0)
+	// 	xdp_flags |= XDP_FLAGS_SKB_MODE;
+	// else
+	// 	xdp_flags |= XDP_FLAGS_DRV_MODE;
+	
+	// err = bpf_xdp_attach(ifindex_list, bpf_program__fd(skel->progs.xdp_pass), xdp_flags, NULL);
+	bpf_program__attach_tracepoint(skel->progs.TCPconnection, "sock", "inet_sock_set_state");
+	bpf_program__attach_tracepoint(skel->progs.sock_error, "sock", "inet_sk_error_report");
+	bpf_program__attach_tracepoint(skel->progs.tcp_synack, "tcp", "tcp_retransmit_synack");
+	// if (ret == -EEXIST && !(xdp_flags & XDP_FLAGS_UPDATE_IF_NOEXIST))
+	// {
+	// 	uint32_t old_flags = xdp_flags;
+
+	// 	xdp_flags &= ~XDP_FLAGS_MODES;
+	// 	xdp_flags |= (old_flags & XDP_FLAGS_SKB_MODE) ? XDP_FLAGS_DRV_MODE : XDP_FLAGS_SKB_MODE;
+	// 	if (bpf_xdp_detach(ifindex_list, xdp_flags, NULL) == 0)
+	// 	{
+	// 		printf("try to attach xdp again...\n");
+	// 		err = bpf_xdp_attach(ifindex_list, prog_fd, old_flags, NULL);
+	// 	}
+	// }
 	/* Attach tracepoints */
-	err = pscannerguard_bpf__attach(skel);
-	if (err) {
+	//err = pscannerguard_bpf__attach(skel);
+	if (err)
+	{
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		return 0;
 	}
-    std::cin.get();
-    pscannerguard_bpf__destroy(skel);
-    return 0;
+	std::cin.get();
+	bpf_xdp_detach(ifindex_list, xdp_flags, NULL);
+	pscannerguard_bpf__destroy(skel);
+	return 0;
 }
